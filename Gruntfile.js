12b384ced5a37cd6bc43855792c0eb8791b99328
Fred Korts <fredkorts@gmail.com>
2017-09-11 09:31:44 +0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		 * Sass
+		 */
+		sass: {
+		  dev: {
+		    options: {
+		      style: 'expanded',
+		      sourcemap: 'none',
+		    },
+		    files: {
+		      'style.css': 'sass/style.scss'
+		    }
+		  },
+		  dist: {
+			  options : {
+				  style: 'compressed',
+				  sourcemap: 'none',
+			  },
+			  files: {
+				  'style-min.css': 'sass/style.scss'
+			  }
+		  }
+		},
+		/**Autoprefixer*/
+		autoprefixer: {
+			options: {
+				browsers: ['last 3 versions']
+			},
+			//prefixes all files
+			multiple_files: {
+				expand: true,
+				flatten: true,
+				src: 'style.css',
+				dest: ''
+			}
+		},
+	  	/**
+	  	 * Watch
+	  	 */
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass','autoprefixer'],
+			},
+		},
+
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.registerTask('default',['watch']);
+}
---
+++ js/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { Tab, Tabs } from 'vue-tabs-component';
+
+new Vue({
+    components: { Tab, Tabs },
+
+    el: '.form-app',
+});
